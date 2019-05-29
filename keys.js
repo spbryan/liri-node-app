e706f2d158ecf68da5afcc5806dfa2a7affce851
spbryan <seanbryan@comcast.com>
2019-05-29 16:54:53 -0400

    Initial commit

---
+++ keys.js
@@ -0,0 +1,12 @@
+/***********************************************
+ * Project: LIRI
+ * Developer: Sean Bryan
+ * Date: 2019-05-29
+ ***********************************************/
+
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
---
+++ liri.js
@@ -0,0 +1,8 @@
+/***********************************************
+ * Project: LIRI
+ * Developer: Sean Bryan
+ * Date: 2019-05-29
+ ***********************************************/
+//Imports
+require("dotenv").config();
+var keys = require("./keys.js");
