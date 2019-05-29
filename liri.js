ac07f1d9f7b7fe78bc6cf004d42a2960e702afb7
spbryan <seanbryan@comcast.com>
2019-05-29 18:50:42 -0400

    add functionality for concert-this

--- liri.js
+++ liri.js
@@ -5,6 +5,8 @@
  ***********************************************/
 //Imports
 require("dotenv").config();
+var axios = require("axios");
+var moment = require("moment");
 var keys = require("./keys.js");
 
 var service = process.argv[2];
@@ -30,27 +32,55 @@ switch (service) {
 /**
  * Process for concert-this selection
  */
-function concertThis () {
-    console.log('boom - concert-this');
+function concertThis() {
+    var queryURL = "https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp";
+    console.log(queryURL);
+    axios.get(queryURL)
+        .then(function (response) {
+            for (var event in response.data) {
+                var venueDetail = createVenueDetail(response.data[event]);
+                console.log(venueDetail);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 /**
  * Process for spotify-this-song selection
  */
-function spotifyThisSong () {
+function spotifyThisSong() {
     console.log('boom - spotify-this-song');
 }
 
 /**
  * Process for movie-this selection
  */
-function movieThis () {
+function movieThis() {
     console.log('boom - movie-this');
 }
 
 /**
  * Process for do-what-it-says selection
  */
-function doWhatItSays () {
+function doWhatItSays() {
     console.log('boom - do-what-it-says');
+}
+
+/**
+ * Build a string with venue details based on API response
+ * @param event 
+ */
+function createVenueDetail(event) {
+    var venueDetail = "";
+    var venueName = event.venue.name;
+    var city = event.venue.city;
+    var region = event.venue.region;
+    var eventDate = moment(event.datetime).format('MM/DD/YYYY');
+    venueDetail = venueName + " on " + eventDate + " - " + city;
+    if (region) {
+        venueDetail += ", " + region;
+    } 
+    return venueDetail;
 }
