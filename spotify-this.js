cde1ec7b6a7cb5fbecdb34ffde5fc2dd3153dbbe
spbryan <seanbryan@comcast.com>
2019-05-29 20:29:38 -0400

    consume spotify API and get back basic response details

--- spotify-this.js
+++ spotify-this.js
@@ -3,14 +3,28 @@
  * Developer: Sean Bryan
  * Date: 2019-05-29
  ***********************************************/
+// require('dotenv').config();
+var Spotify = require('node-spotify-api');
 
- /**
- * Process for spotify-this-song selection
- */
+var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+});
+
+/**
+* Process for spotify-this-song selection
+*/
 function spotifyThisSong(songName) {
-    console.log('boom - spotify-this-song');
+    spotify
+        .search({ type: 'artist', query: songName })
+        .then(function (response) {
+            console.log(response.artists.items);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
 module.exports = {
-    spotifyThisSong : spotifyThisSong
+    spotifyThisSong: spotifyThisSong
 }
