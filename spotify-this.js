64a1160a6f384a1a50073ad9debd9f95dfba504c
spbryan <seanbryan@comcast.com>
2019-05-31 18:51:45 -0400

    get and display song track details from spotify API

--- spotify-this.js
+++ spotify-this.js
@@ -3,7 +3,6 @@
  * Developer: Sean Bryan
  * Date: 2019-05-29
  ***********************************************/
-// require('dotenv').config();
 var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify({
@@ -16,13 +15,41 @@ var spotify = new Spotify({
 */
 function spotifyThisSong(songName) {
     spotify
-        .search({ type: 'artist', query: songName })
+        .search({ type: 'track', query: songName })
         .then(function (response) {
-            console.log(response.artists.items);
+            for (var item in response.tracks.items) {
+                var songDetail = displaySongDetail(response.tracks.items[item]);
+            }
         })
         .catch(function (err) {
             console.log(err);
         });
+}
+
+/**
+ * Build a string with song details based on API response
+ * @param item 
+ */
+function displaySongDetail(item) {
+    console.log("Song Title: " + item.name);
+    console.log("Album: " + item.album.name);
+    var artistNames = getArtistNames(item.artists);
+    console.log("Artist(s): " + artistNames.join(", "));
+    console.log("Spotify Link: " + item.external_urls.spotify);
+    console.log("+-----------------------------------------------------------------------+");
+}
+
+/**
+ * Build an array of artist names with values taken from
+ * artists object
+ * @param artists 
+ */
+function getArtistNames(artists) {
+    var artistNames = [];
+    for (var artist in artists) {
+        artistNames.push(artists[artist].name);
+    }
+    return artistNames;
 }
 
 module.exports = {
